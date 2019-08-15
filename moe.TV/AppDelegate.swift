66dd7e0905f47bb06a758e2e45f64ecc9dc7a99a
bi119aTe5hXk <billgateshxk@gmail.com>
2019-08-16 01:10:25 +0900

    compete all URL

--- moe.TV/AppDelegate.swift
+++ moe.TV/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UserDefaults.standard.register(defaults: ["savedCookies" : []])
+        UserDefaults.standard.register(defaults: ["serveraddr" : ""])
         
         if let tabController = window?.rootViewController as? UITabBarController {
             tabController.viewControllers?.append(packagedSearchController())
--- moe.TV/ServerHandler.swift
+++ moe.TV/ServerHandler.swift
@@ -10,38 +10,110 @@ import Alamofire
 import Foundation
 
 func getServerAddr() -> String {
-    return UserDefaults.standard.string(forKey: "serveraddr")!
+    var urlStr = UserDefaults.standard.string(forKey: "serveraddr")!
+    urlStr.append("/api")
+    return urlStr
 }
-func logInServer(username:String, password:String, completion: @escaping (Any?, String?) -> Void) {
+func logInServer(username:String, password:String, completion: @escaping (Any?) -> Void) {
     var urlstr = getServerAddr()
-    urlstr.append("/api/user/login")
+    urlstr.append("/user/login")
     
     let postdata = ["name":username, "password":password, "remmember":true] as [String : Any]
     
     AF.request(urlstr, method: .post, parameters: postdata, encoding: JSONEncoding.default).responseJSON { response in
+        
+        //save cookies from response
+        saveCookies(response: response)
+        
+        
         print(response.result)
+        //completion(response.result)
     }
 }
 
 
-func getMyBangumiList(completion: @escaping (Any?, String?) -> Void) {
+func getMyBangumiList(completion: @escaping (Any?) -> Void) {
     var urlstr = getServerAddr()
-    urlstr.append("/api/home/my_bangumi?status=3")
-}
-
-func getOnAirList(completion: @escaping (Any?, String?) -> Void) {
+    urlstr.append("/home/my_bangumi?status=3")
+    loadCookies()
+    AF.request(urlstr, method: .get, encoding: JSONEncoding.default).responseJSON { response in
+        
+        print(response.result)
+        //completion(response.result)
+    }
     
 }
 
-func getAllBangumiList(page:Int, name:String, completion: @escaping (Any?, String?) -> Void) {
+func getOnAirList(completion: @escaping (Any?) -> Void) {
+    var urlstr = getServerAddr()
+    urlstr.append("/home/on_air")
+    loadCookies()
+    AF.request(urlstr, method: .get, encoding: JSONEncoding.default).responseJSON { response in
+        
+        print(response.result)
+        //completion(response.result)
+    }
+}
+
+func getAllBangumiList(page:Int, name:String, completion: @escaping (Any?) -> Void) {
+    var urlstr = getServerAddr()
+    urlstr.append("/home/bangumi?page=")
+    urlstr.append(String(page))
+    urlstr.append("&count=10&sort_field=air_date&sort_order=desc&name=")
+    urlstr.append(name)
+    urlstr.append("&type=-1")
+    loadCookies()
+    
+    AF.request(urlstr, method: .get, encoding: JSONEncoding.default).responseJSON { response in
+        
+        print(response.result)
+        //completion(response.result)
+    }
     
 }
-func getBangumiDetail(id:String, completion: @escaping (Any?, String?) -> Void) {
+func getBangumiDetail(id:String, completion: @escaping (Any?) -> Void) {
+    var urlstr = getServerAddr()
+    urlstr.append("/home/bangumi/")
+    urlstr.append(id)
+    loadCookies()
     
+    AF.request(urlstr, method: .get, encoding: JSONEncoding.default).responseJSON { response in
+        
+        print(response.result)
+        //completion(response.result)
+    }
 }
-func getEpisodeDetail(ep_id:String,completion: @escaping (Any?, String?) -> Void) {
+func getEpisodeDetail(ep_id:String,completion: @escaping (Any?) -> Void) {
+    var urlstr = getServerAddr()
+    urlstr.append("/home/episode/")
+    urlstr.append(ep_id)
+    loadCookies()
     
+    AF.request(urlstr, method: .get, encoding: JSONEncoding.default).responseJSON { response in
+        
+        print(response.result)
+        //completion(response.result)
+    }
 }
 
 
 
+func saveCookies(response: DataResponse<Any>) {
+    let headerFields = response.response?.allHeaderFields as! [String: String]
+    let url = response.response?.url
+    let cookies = HTTPCookie.cookies(withResponseHeaderFields: headerFields, for: url!)
+    var cookieArray = [[HTTPCookiePropertyKey: Any]]()
+    for cookie in cookies {
+        cookieArray.append(cookie.properties!)
+    }
+    UserDefaults.standard.set(cookieArray, forKey: "savedCookies")
+    UserDefaults.standard.synchronize()
+}
+func loadCookies() {
+    guard let cookieArray = UserDefaults.standard.array(forKey: "savedCookies") as? [[HTTPCookiePropertyKey: Any]] else { return }
+    for cookieProperties in cookieArray {
+        if let cookie = HTTPCookie(properties: cookieProperties) {
+            HTTPCookieStorage.shared.setCookie(cookie)
+        }
+    }
+}
