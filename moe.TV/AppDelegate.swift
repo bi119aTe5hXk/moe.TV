2105b62aee450cf8443a1d8904834fe484d498ee
bi119aTe5hXk <billgateshxk@gmail.com>
2019-08-16 02:47:10 +0900

    wtf?

---
+++ moe.TV/AppDelegate.swift
@@ -0,0 +1,82 @@
+//
+//  AppDelegate.swift
+//  moe.TV
+//
+//  Created by billgateshxk on 2019/08/13.
+//  Copyright © 2019 bi119aTe5hXk. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        UserDefaults.standard.register(defaults: ["savedCookies" : []])
+        UserDefaults.standard.register(defaults: ["loggedin" : false])
+        UserDefaults.standard.register(defaults: ["serveraddr" : ""])
+        
+        if let tabController = window?.rootViewController as? UITabBarController {
+            tabController.viewControllers?.append(packagedSearchController())
+        }
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+    // MARK: Convenience
+    
+    /*
+     A method demonstrating how to encapsulate a `UISearchController` for presentation in, for example, a `UITabBarController`
+     */
+    func packagedSearchController() -> UIViewController {
+        // Load a `SearchResultsViewController` from its storyboard.
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let searchResultsController = storyboard.instantiateViewController(withIdentifier: SearchResultsViewController.storyboardIdentifier) as? SearchResultsViewController else {
+            fatalError("Unable to instatiate a SearchResultsViewController from the storyboard.")
+        }
+        
+        /*
+         Create a UISearchController, passing the `searchResultsController` to
+         use to display search results.
+         */
+        let searchController = UISearchController(searchResultsController: searchResultsController)
+        searchController.searchResultsUpdater = searchResultsController
+        searchController.searchBar.placeholder = NSLocalizedString("Enter keyword (e.g. iceland)", comment: "")
+        
+        // Contain the `UISearchController` in a `UISearchContainerViewController`.
+        let searchContainer = UISearchContainerViewController(searchController: searchController)
+        searchContainer.title = NSLocalizedString("Search", comment: "")
+        
+        // Finally contain the `UISearchContainerViewController` in a `UINavigationController`.
+        let searchNavigationController = UINavigationController(rootViewController: searchContainer)
+        return searchNavigationController
+    }
+}
+
---
+++ moe.TV/SplitViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SplitViewController.swift
+//  moe.TV
+//
+//  Created by bi119aTe5hXk on 2019/08/16.
+//  Copyright © 2019 bi119aTe5hXk. All rights reserved.
+//
+
+import UIKit
+
+
+class SplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
