ac90c488f979860d4d27ed875b371a0e8967eba2
bi119aTe5hXk <billgateshxk@gmail.com>
2020-03-12 04:04:54 +0900

    better code, better life

--- moe.TV/AppDelegate.swift
+++ moe.TV/AppDelegate.swift
@@ -15,20 +15,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        UserDefaults.init(suiteName: UD_SUITE_NAME)?.register(defaults: [UD_SAVED_COOKIES : []])
-        UserDefaults.init(suiteName: UD_SUITE_NAME)?.register(defaults: [UD_TOPSHELF_ARR : []])
-        UserDefaults.init(suiteName: UD_SUITE_NAME)?.register(defaults: [UD_LOGEDIN : false])
-        UserDefaults.init(suiteName: UD_SUITE_NAME)?.register(defaults: [UD_SERVICE_TYPE : "albireo"])
-        UserDefaults.init(suiteName: UD_SUITE_NAME)?.register(defaults: [UD_SERVER_ADDR : ""])
-        UserDefaults.init(suiteName: UD_SUITE_NAME)?.register(defaults: [UD_PROXY_SERVER : ""])
-        UserDefaults.init(suiteName: UD_SUITE_NAME)?.register(defaults: [UD_PROXY_PORT : ""])
-        UserDefaults.init(suiteName: UD_SUITE_NAME)?.register(defaults: [UD_SONARR_APIKEY : ""])
-        UserDefaults.init(suiteName: UD_SUITE_NAME)?.register(defaults: [UD_SONARR_USERNAME : ""])
-        UserDefaults.init(suiteName: UD_SUITE_NAME)?.register(defaults: [UD_SONARR_PASSWORD : ""])
-        UserDefaults.init(suiteName: UD_SUITE_NAME)?.register(defaults: [UD_SONARR_ROOTFOLDER : ""])
-        UserDefaults.init(suiteName: UD_SUITE_NAME)?.register(defaults: [UD_USING_HTTPS : true])
-        UserDefaults.init(suiteName: UD_SUITE_NAME)?.register(defaults: [UD_SONARR_WEBDAV_PORT : 0])
-        UserDefaults.init(suiteName: UD_SUITE_NAME)?.register(defaults: [UD_SONARR_USINGBASICAUTH : false])
+        
+        let ud = UserDefaults.init(suiteName: UD_SUITE_NAME)!
+        ud.register(defaults: [UD_SAVED_COOKIES : []])
+        ud.register(defaults: [UD_TOPSHELF_ARR : []])
+        ud.register(defaults: [UD_LOGEDIN : false])
+        ud.register(defaults: [UD_SERVICE_TYPE : "albireo"])
+        ud.register(defaults: [UD_SERVER_ADDR : ""])
+        ud.register(defaults: [UD_PROXY_SERVER : ""])
+        ud.register(defaults: [UD_PROXY_PORT : ""])
+        ud.register(defaults: [UD_SONARR_APIKEY : ""])
+        ud.register(defaults: [UD_SONARR_USERNAME : ""])
+        ud.register(defaults: [UD_SONARR_PASSWORD : ""])
+        ud.register(defaults: [UD_SONARR_ROOTFOLDER : ""])
+        ud.register(defaults: [UD_USING_HTTPS : true])
+        ud.register(defaults: [UD_SONARR_WEBDAV_PORT : 0])
+        ud.register(defaults: [UD_SONARR_USINGBASICAUTH : false])
         
         //UserDefaults.init(suiteName: UD_SUITE_NAME)?.set(false, forKey: UD_LOGEDIN)
         
