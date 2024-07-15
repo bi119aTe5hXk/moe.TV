9e58a4b149a0fe1b9f5038a8a3352a8ea0438a4b
bi119aTe5hXk <billgateshxk@gmail.com>
2024-07-15 12:37:35 +0900

    Update ci_pre_xcodebuild.sh

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -2,10 +2,6 @@
 
 echo "Stage: PRE-Xcode Build is activated .... "
 
-# Move to the place where the scripts are located.
-# This is important because the position of the subsequently mentioned files depend of this origin.
-cd $CI_WORKSPACE/ci_scripts || exit 1
-
 # Write a Swift File containing all the environment variables and secrets.
 printf "let bgmAppID = \"%s\"\nlet bgmAppSecret = \"%s\" " "$bgmAppID" "$bgmAppSecret" >> ../moe.TV/DONOTUPLOAD.swift
 
